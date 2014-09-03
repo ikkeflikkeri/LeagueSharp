746733d36261d684274e1b5ca62ebfcd7d893fe1
Niels <niels@woutwoot.be>
2014-09-03 20:00:32 +0200

    Initial commit

---
+++ EasyXerath/EasyXerath/Program.cs
@@ -0,0 +1,38 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyXerath
+{
+    class Program
+    {
+        private static Champion Xerath;
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            Xerath = new Xerath();
+            if (!Xerath.isLoaded) return;
+
+            Game.OnGameUpdate += Game_OnGameUpdate;
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+        static void Drawing_OnDraw(EventArgs args)
+        {
+            Xerath.Drawing();
+        }
+
+        static void Game_OnGameUpdate(EventArgs args)
+        {
+            Xerath.Update();
+        }
+    }
+}
---
+++ EasyXerath/EasyXerath/Xerath.cs
@@ -0,0 +1,45 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace EasyXerath
+{
+    class Xerath : Champion
+    {
+        public Xerath() : base("Xerath")
+        {
+            
+        }
+
+        protected override void CreateSpells()
+        {
+            
+        }
+
+        protected override void CreateMenu()
+        {
+            
+        }
+
+        protected override void Combo()
+        {
+            
+        }
+        protected override void Harass()
+        {
+            
+        }
+        protected override void Auto()
+        {
+            
+        }
+        public override void Drawing()
+        {
+            
+        }
+    }
+}
