3d9e61564312da04597abd0434d30404ee5addbe
Niels <niels@woutwoot.be>
2014-09-03 14:55:24 +0200

    Initial commit

---
+++ EasyKogMaw/EasyKogMaw/KogMaw.cs
@@ -0,0 +1,44 @@
+﻿using LeagueSharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyKogMaw
+{
+    class KogMaw : Champion
+    {
+        public KogMaw() : base("KogMaw")
+        {
+            
+        }
+
+        protected override void CreateSpells()
+        {
+            
+        }
+
+        protected override void CreateMenu()
+        {
+            
+        }
+
+        protected override void Combo()
+        {
+           
+        }
+        protected override void Harass()
+        {
+            
+        }
+        protected override void Auto()
+        {
+            
+        }
+        public override void Drawing()
+        {
+           
+        }
+    }
+}
---
+++ EasyKogMaw/EasyKogMaw/Program.cs
@@ -0,0 +1,38 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyKogMaw
+{
+    class Program
+    {
+        private static KogMaw KogMaw;
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            KogMaw = new KogMaw();
+            if (!KogMaw.isLoaded) return;
+
+            Game.OnGameUpdate += Game_OnGameUpdate;
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+        static void Drawing_OnDraw(EventArgs args)
+        {
+            KogMaw.Drawing();
+        }
+
+        static void Game_OnGameUpdate(EventArgs args)
+        {
+            KogMaw.Update();
+        }
+    }
+}
