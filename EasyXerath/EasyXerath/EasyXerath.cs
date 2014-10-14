5011aaf9650034f3d4aa0768d2cce256e6ce66ce
Niels <niels@woutwoot.be>
2014-10-14 23:08:16 +0200

    Added xerath wip

---
+++ EasyXerath/EasyXerath/EasyXerath.cs
@@ -0,0 +1,192 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyXerath
+{
+    class EasyXerath : Champion
+    {
+        static void Main(string[] args)
+        {
+            new EasyXerath();
+        }
+
+        public EasyXerath() : base("Xerath")
+        {
+            Drawing.OnEndScene += Drawing_OnEndScene;
+        }
+
+        void Drawing_OnEndScene(EventArgs args)
+        {
+            Utility.DrawCircle(Player.Position, Spells.get("R").Range, Color.FromArgb(255, 255, 255), 1, 30, true);
+        }
+
+        protected override void InitializeSkins(ref SkinManager Skins)
+        {
+            Skins.Add("Xerath");
+            Skins.Add("Runeborn Xerath");
+            Skins.Add("Battlecast Xerath");
+            Skins.Add("Scorched Earth Xerath");
+        }
+        protected override void InitializeSpells(ref SpellManager Spells)
+        {
+            Spell Q = new Spell(SpellSlot.Q, 1600f);
+            Q.SetSkillshot(0.6f, 100f, float.MaxValue, false, SkillshotType.SkillshotLine);
+            Q.SetCharged("XerathArcanopulseChargeUp", "XerathArcanopulseChargeUp", 750, 1550, 1.5f);
+
+            Spell W = new Spell(SpellSlot.W, 1000f);
+            W.SetSkillshot(0.7f, 200f, float.MaxValue, false, SkillshotType.SkillshotCircle);
+
+            Spell WCenter = new Spell(SpellSlot.W, 1000f);
+            WCenter.SetSkillshot(0.7f, 50f, float.MaxValue, false, SkillshotType.SkillshotCircle);
+
+            Spell E = new Spell(SpellSlot.E, 1150f);
+            E.SetSkillshot(0.2f, 60, 1400f, true, SkillshotType.SkillshotLine);
+
+            Spell R = new Spell(SpellSlot.R, 2950);
+            R.SetSkillshot(0.7f, 120f, float.MaxValue, false, SkillshotType.SkillshotCircle);
+
+            Spells.Add("Q", Q);
+            Spells.Add("W", W);
+            Spells.Add("WCenter", WCenter);
+            Spells.Add("E", E);
+            Spells.Add("R", R);
+        }
+        protected override void InitializeMenu()
+        {
+            Menu.AddSubMenu(new Menu("Combo", "Combo"));
+            Menu.SubMenu("Combo").AddItem(new MenuItem("Combo_q", "Use Q").SetValue(true));
+            Menu.SubMenu("Combo").AddItem(new MenuItem("Combo_w", "Use W").SetValue(true));
+            Menu.SubMenu("Combo").AddItem(new MenuItem("Combo_e", "Use E").SetValue(true));
+
+            Menu.AddSubMenu(new Menu("Harass", "Harass"));
+            Menu.SubMenu("Harass").AddItem(new MenuItem("Harass_q", "Use Q").SetValue(true));
+            Menu.SubMenu("Harass").AddItem(new MenuItem("Harass_w", "Use W").SetValue(false));
+            Menu.SubMenu("Harass").AddItem(new MenuItem("Harass_e", "Use E").SetValue(false));
+
+            Menu.AddSubMenu(new Menu("Auto", "Auto"));
+            Menu.SubMenu("Auto").AddItem(new MenuItem("Auto_q", "Use Q").SetValue(false));
+            Menu.SubMenu("Auto").AddItem(new MenuItem("Auto_w", "Use W").SetValue(false));
+            Menu.SubMenu("Auto").AddItem(new MenuItem("Auto_e", "Use E").SetValue(false));
+
+            Menu.AddSubMenu(new Menu("Drawing", "Drawing"));
+            Menu.SubMenu("Drawing").AddItem(new MenuItem("Drawing_q", "Use Q").SetValue(new Circle(true, Color.FromArgb(100, 0, 255, 0))));
+            Menu.SubMenu("Drawing").AddItem(new MenuItem("Drawing_w", "Use W").SetValue(new Circle(true, Color.FromArgb(100, 0, 255, 0))));
+            Menu.SubMenu("Drawing").AddItem(new MenuItem("Drawing_e", "Use E").SetValue(new Circle(true, Color.FromArgb(100, 0, 255, 0))));
+            Menu.SubMenu("Drawing").AddItem(new MenuItem("Drawing_r", "Use R").SetValue(new Circle(true, Color.FromArgb(100, 0, 255, 0))));
+            Menu.SubMenu("Drawing").AddItem(new MenuItem("Drawing_rdamage", "R damage indicator").SetValue(true));
+
+            Menu.AddSubMenu(new Menu("Misc", "Misc"));
+            Menu.SubMenu("Misc").AddItem(new MenuItem("Misc_wcenter", "Cast W centered").SetValue(true));
+            Menu.SubMenu("Misc").AddItem(new MenuItem("Misc_stun", "Use E").SetValue<KeyBind>(new KeyBind("T".ToCharArray()[0], KeyBindType.Press)));
+            Menu.SubMenu("Misc").AddItem(new MenuItem("Misc_ult", "Use R when ult is activated").SetValue(true));
+        }
+
+        protected override void Combo()
+        {
+            if (Menu.Item("Combo_w").GetValue<bool>()) 
+                if (Menu.Item("Misc_wcenter").GetValue<bool>())
+                    Spells.CastSkillshot("WCenter", SimpleTs.DamageType.Magical, HitChance.High);
+                else
+                    Spells.CastSkillshot("W", SimpleTs.DamageType.Magical, HitChance.High);
+
+            if (Menu.Item("Combo_e").GetValue<bool>()) Spells.CastSkillshot("E", SimpleTs.DamageType.Magical);
+            if (Menu.Item("Combo_q").GetValue<bool>()) Spells.CastSkillshot("Q", SimpleTs.DamageType.Magical);
+        }
+        protected override void Harass()
+        {
+            if (Menu.Item("Harass_w").GetValue<bool>())
+                if (Menu.Item("Misc_wcenter").GetValue<bool>())
+                    Spells.CastSkillshot("WCenter", SimpleTs.DamageType.Magical, HitChance.High);
+                else
+                    Spells.CastSkillshot("W", SimpleTs.DamageType.Magical, HitChance.High);
+
+            if (Menu.Item("Harass_e").GetValue<bool>()) Spells.CastSkillshot("E", SimpleTs.DamageType.Magical);
+            if (Menu.Item("Harass_q").GetValue<bool>()) Spells.CastSkillshot("Q", SimpleTs.DamageType.Magical);
+        }
+        protected override void Auto()
+        {
+            if (Menu.Item("Misc_stun").GetValue<KeyBind>().Active) Spells.CastSkillshot("E", SimpleTs.DamageType.Magical);
+
+            if (Menu.Item("Auto_w").GetValue<bool>())
+                if (Menu.Item("Misc_wcenter").GetValue<bool>())
+                    Spells.CastSkillshot("WCenter", SimpleTs.DamageType.Magical, HitChance.High);
+                else
+                    Spells.CastSkillshot("W", SimpleTs.DamageType.Magical, HitChance.High);
+
+            if (Menu.Item("Auto_e").GetValue<bool>()) Spells.CastSkillshot("E", SimpleTs.DamageType.Magical);
+            if (Menu.Item("Auto_q").GetValue<bool>()) Spells.CastSkillshot("Q", SimpleTs.DamageType.Magical);
+
+            if (Menu.Item("Misc_ult").GetValue<bool>() && Player.HasBuff("XerathR"))
+            {
+                Console.WriteLine("Ultimate");
+                CastR();
+            }
+            else
+            {
+                RTarget = null;
+                RChange = 0;
+            }
+        }
+
+        protected override void Draw()
+        {
+            DrawCircle("Drawing_q", "Q");
+            DrawCircle("Drawing_w", "W");
+            DrawCircle("Drawing_e", "E");
+            DrawCircle("Drawing_r", "R");
+
+            Utility.HpBarDamageIndicator.DamageToUnit = UltimateDamage;
+            Utility.HpBarDamageIndicator.Enabled = Menu.Item("Drawing_rdamage").GetValue<bool>();
+        }
+        protected override void Update()
+        {
+            if (Spells.get("R").Level > 0)
+                Spells.get("R").Range = 1750 + Spells.get("R").Level * 1200;
+        }
+
+        private Obj_AI_Hero RTarget = null;
+        private int RChange = 0;
+
+        private void CastR()
+        {
+            Spell R = Spells.get("R");
+
+            Obj_AI_Hero target = SimpleTs.GetTarget(R.Range, SimpleTs.DamageType.Magical);
+            if (target == null) return;
+
+            Console.WriteLine("CastR");
+
+            if(RTarget != null && target.Position != RTarget.Position)
+            {
+                Console.WriteLine("CastR3 - " + RTarget.Distance(target) / 2.5f);
+
+                if (Environment.TickCount < RChange + (int)(RTarget.Distance(target) / 2.5f))
+                {
+                    Console.WriteLine("Wait");
+                    return;
+                }
+            }
+            Console.WriteLine("Cast");
+
+            if (target.IsValidTarget(R.Range) && R.GetPrediction(target).Hitchance >= HitChance.VeryHigh)
+            {
+                R.Cast(target, true, true);
+                RTarget = target;
+                RChange = Environment.TickCount + 600;
+            }
+        }
+
+        private float UltimateDamage(Obj_AI_Hero hero)
+        {
+            if(Spells.get("R").Level > 0)
+                return (float)Damage.GetSpellDamage(Player, hero, SpellSlot.R) * 2.95f;
+            return 0f;
+        }
+    }
+}
---
+++ EasyXerath/EasyXerath/SpellManager.cs
@@ -0,0 +1,98 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SpellManager
+{
+    private Dictionary<string, Spell> Spells = new Dictionary<string, Spell>();
+
+    public SpellManager()
+    {
+
+    }
+
+    public void Add(string name, Spell spell)
+    {
+        Spells.Add(name, spell);
+    }
+
+    public Spell get(string name)
+    {
+        return Spells[name];
+    }
+
+    public void CastSkillshot(string spell, Obj_AI_Base target, HitChance hitChance = HitChance.VeryHigh, bool packet = true, bool aoe = false)
+    {
+        if (!Spells[spell].IsReady()) return;
+
+        if (!Spells[spell].IsChargedSpell)
+        {
+            if (target.IsValidTarget(Spells[spell].Range) && Spells[spell].GetPrediction(target).Hitchance >= hitChance)
+                Spells[spell].Cast(target, packet, aoe);
+        }
+        else
+        {
+            if (!target.IsValidTarget(Spells[spell].Range)) return;
+
+            if (!Spells[spell].IsCharging)
+                Spells[spell].StartCharging();
+            else
+            {
+                if (hitChance == HitChance.VeryHigh && Spells[spell].Range - Spells[spell].ChargedMaxRange * 0.2f > ObjectManager.Player.Distance(target) && Spells[spell].GetPrediction(target).Hitchance >= HitChance.High)
+                    Spells[spell].Cast(target, packet, aoe);
+                if (Spells[spell].GetPrediction(target).Hitchance >= hitChance)
+                    Spells[spell].Cast(target, packet, aoe);
+            }
+        }
+    }
+    public void CastSkillshot(string spell, SimpleTs.DamageType damageType, HitChance hitChance = HitChance.VeryHigh, bool packet = true, bool aoe = false)
+    {
+        if (!Spells[spell].IsReady()) return;
+
+        if (!Spells[spell].IsChargedSpell)
+        {
+            Obj_AI_Hero target = SimpleTs.GetTarget(Spells[spell].Range, damageType);
+            if (target == null) return;
+
+            if (target.IsValidTarget(Spells[spell].Range) && Spells[spell].GetPrediction(target).Hitchance >= hitChance)
+                Spells[spell].Cast(target, packet, aoe);
+        }
+        else
+        {
+            Obj_AI_Hero target = SimpleTs.GetTarget(Spells[spell].ChargedMaxRange, damageType);
+            if (target == null || !target.IsValidTarget(Spells[spell].ChargedMaxRange))
+                return;
+
+            if (!Spells[spell].IsCharging)
+                Spells[spell].StartCharging();
+            else
+            {
+                if (hitChance == HitChance.VeryHigh && Spells[spell].Range - Spells[spell].ChargedMaxRange * 0.2f > ObjectManager.Player.Distance(target) && Spells[spell].GetPrediction(target).Hitchance >= HitChance.High)
+                    Spells[spell].Cast(target, packet, aoe);
+                if (Spells[spell].GetPrediction(target).Hitchance >= hitChance)
+                    Spells[spell].Cast(target, packet, aoe);
+            }
+        }
+    }
+    public void CastOnTarget(string spell, Obj_AI_Base target, bool packet = true)
+    {
+        if (!Spells[spell].IsReady()) return;
+
+        if (target.IsValidTarget(Spells[spell].Range))
+            Spells[spell].CastOnUnit(target, packet);
+    }
+    public void CastOnTarget(string spell, SimpleTs.DamageType damageType, bool packet = true)
+    {
+        if (!Spells[spell].IsReady()) return;
+
+        Obj_AI_Hero target = SimpleTs.GetTarget(Spells[spell].Range, damageType);
+        if (target == null) return;
+
+        if (target.IsValidTarget(Spells[spell].Range))
+            Spells[spell].CastOnUnit(target, packet);
+    }
+}
